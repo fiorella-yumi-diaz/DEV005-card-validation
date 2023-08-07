75483fe9c52aa88edae17246ff36318f12b8bfc3
Fiorella Diaz <fiorella.dm2002@gmail.com>
2023-08-07 15:32:17 -0500

    mejora de estilos y js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import validator from './validator.js';
 
-//Elemetos DOM en variables
-//const vistaBoton1 = document.getElementById('');
-//const vistaBoton2 = document.getElementById('');
-//const vistaBoton3 = document.getElementById('');
-//const vistaCaja1 = document.getElementById('');
+// Selector del Dom
 const ValorVistaValidar =document.getElementById('cajaVistaInputs');
 const inputValorNombre = document.getElementById('inputValorNombre');
 const valorInputCard = document.getElementById('inputValorCard');
@@ -14,21 +10,10 @@ const valorVistaResultado= document.getElementById('cajaResultadoVista');
 const valorTituloResultadoTrue = document.getElementById('tituloValorVerdadero');
 const valorResultadoNombre = document.getElementById('ouputNombre');
 const valorResultadoCard =  document.getElementById('ouputCard');
-//const targets = document.getElementById('[data-target]');
-//const content = document.getElementById('[data-content]');
+const btnReturn =  document.getElementById("btn-return");
+const formulario = document.getElementsByClassName("form");
 
-//Funciones que interactuen con el DOM
-/*
-targets.forEach(target => {
-  target.addEventListener("click", () => {
-    content.forEach(c => {
-      c.classList.remove('active');
-    })
-    const t = document.querySelector(target.dataset.target);
-    t.classList.add("active")
-  })
-});
-*/
+// Resultado correcto de la tarjeta
 function mostrarExitoso(nombre, cardMaskify) { 
   ValorVistaValidar.style.display = "none";
   valorResultadoFalso.style.display = "none";
@@ -38,27 +23,31 @@ function mostrarExitoso(nombre, cardMaskify) {
   valorTituloResultadoTrue.innerHTML = "Donacion exitosa";
 }
 
+// Resultado falso de la tarjeta
 function mostrarFallido() {
   valorVistaResultado.style.display = "none"
-  valorResultadoFalso.innerHTML = "Tarjeta invalida"
+  valorResultadoFalso.innerHTML = "Invalid card"
 }
 
+// Funcion que muestra a tarjeta resultado
 function mostrarValidacionCard() {
   const isValid = validator.isValid(valorInputCard.value);
-  if (isValid === true) {//faltacompletar
-    const cardNumberMaskify = validator.maskify(valorInputCard.value);
+  const cardNumberMaskify = validator.maskify(valorInputCard.value);
+  if (isValid === true) {
     mostrarExitoso(inputValorNombre.value, cardNumberMaskify)
-
   } else {
     mostrarFallido() 
   }
 }
-
-//Eventos del DOM
-//vistaBoton1.addEventListener('click',);
-//vistaBoton2.addEventListener('click', );
-//vistaBoton3.addEventListener('click',);
 valorBotonValidar.addEventListener('click',mostrarValidacionCard);
+btnReturn.addEventListener('click', function name(event) {
+  event.preventDefault();
+  ValorVistaValidar.style.display = "flex";
+  valorVistaResultado.style.display = "none";
+  console.log("PRUEBA");
 
-//retorna el valor
-console.log(validator);
+
+  
+  
+
+})
--- src/validator.js
+++ src/validator.js
@@ -1,64 +1,53 @@
-//funcion que solo pasa solo numeros para aportar al algoritomo
+// Quita los campos vacios y junta todo
 function isEmptyOrNull(str) { 
   return str.trim().length === 0 || str === null  
 }
+
+// Verificar que los input sean solo numeros
 function isNumber(str) {
   return !isNaN(str)
 }
-//validaciones para el algoritmo lunh en 4 pasos 
+
+// algoritmo lunh 
 function isCardValid(cardNumber) {
-
-  //1. Revertir string
-  console.log('cardNumber: ', typeof cardNumber, cardNumber)
   const arrayNumber = cardNumber.split(""); //separa los numeros
-  console.log('arrayNumber: ', typeof arrayNumber, arrayNumber);
   const reverceArray = arrayNumber.reverse() //revierte los numeros
-  console.log('reverceArray: ', typeof reverceArray, reverceArray);
-  const reversaNumero = reverceArray.join('') //junta los numeros
-  console.log('juntar Numero: ', typeof reversaNumero, reversaNumero);
-
-  //2. Determinar pares
+  // Determinar pares
   let sumaPares = 0; //contador de suma de los pares
   let sumaImpares = 0; //contador de la suma de los impares
   for (let i = 0; i < reverceArray.length; i++) {
     const numero = parseInt(reverceArray[i]);
-    // pocicion par 
     if ((i % 2) === 0) { 
       sumaPares = sumaPares + numero;
-      console.log("suma par : ", sumaPares, i,numero )
-    } else { //posicion inpar
+    } else {
       const operacionMultiplicar = numero * 2;
       if (operacionMultiplicar >= 10) {
         const sumador = ((operacionMultiplicar - 10) + 1);
         sumaImpares = sumaImpares + sumador;
-        console.log("suma impar mayor a 10 : ", sumaImpares, i,numero, sumador )
       } else {
         sumaImpares = sumaImpares + operacionMultiplicar;
-        console.log("suma impar menor a 10 : ", sumaImpares, i,numero )
       }
     }
   }
-  console.log("suma pares" + sumaPares);
-  console.log("suma inpares" + sumaImpares );
+
   //Suma final
-  const sumaTotal = sumaPares+sumaImpares;
+  const sumaTotal = sumaPares + sumaImpares;
   const MOD = sumaTotal%10;
-  if(MOD===0){
+  if(MOD === 0){
     return true;
   } else {
     return false;
   }
 }
 
-//validator.maskify esta funcion es para cifrar todos los numeros menos los ultimos digitos
+// Cifra todos los numeros menos los ultimos digitos
 function maskify(sum) {
-  //1234 7856
   const maskifyString =
   sum.slice(0, -4).replace(/./g, "#") + ("" + sum).slice(-4);
   return maskifyString;
 }
 
-//unificar las funciones para llevarlos a mi index js
+// Objeto validador 
 const validator = {
   isValid : function(cardNumber) {
     if (isEmptyOrNull(cardNumber)) {
